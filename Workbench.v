cf516991a6618ff9859bc72480890a52f178ae96
Yoshimi-Theophile <yoshimitheophile.etienne@gmail.com>
2025-03-04 10:01:50 +0100

    commit

--- Workbench.v
+++ Workbench.v
@@ -6,7 +6,6 @@ Require Import Types.
 
 Require Import Coq.Program.Equality.
 Require Import Arith.
-Require Import Coq.Logic.JMeq.
 Require List.
 
 Definition context n : Type := snoclist type n.
@@ -54,87 +53,22 @@ match sigma with
   subst_ctx sigma' (subst1_ctx rul ctx)
 end.
 
-
-Inductive pterm_eq : forall {a : nat}, pterm a -> pterm a -> Prop :=
-| eq_var : pterm_eq PVar PVar
-| eq_abs {n} (tl tr : pterm (S n)) :
-  pterm_eq tl tr -> pterm_eq (PAbs tl) (PAbs tr)
-| eq_app {n m} (tl1 : pterm n) (tl2 : pterm m) (tr1 : pterm n) (tr2 : pterm m) :
-  pterm_eq tl1 tr1 -> pterm_eq tl2 tr2 -> pterm_eq (PApp tl1 tl2) (PApp tr1 tr2)
-| eq_sym {n} (tl tr : pterm n) : pterm_eq tl tr -> pterm_eq tr tl
-| eq_trans {n} (tl tm tr : pterm n) :
-  pterm_eq tl tm -> pterm_eq tm tr -> pterm_eq tl tr.
-
-Lemma pt_eq : forall {a : nat} (t1 t2 : pterm a), t1 = t2 <-> pterm_eq t1 t2.
-Proof.
-intros a t1 t2. split.
-- intro eq. 
-  induction t2.
-  + rewrite eq. apply eq_var.
-  + rewrite eq.
-    apply (eq_abs t2 t2 (IHt2 t2 eq_refl)).
-  + rewrite eq.
-    apply (eq_app t2_1 t2_2 t2_1 t2_2 (IHt2_1 t2_1 eq_refl) (IHt2_2 t2_2 eq_refl)).
-- intro pt_eq.
-  induction pt_eq.
-  + apply eq_refl.
-  + rewrite IHpt_eq. apply eq_refl.
-  + rewrite IHpt_eq1. rewrite IHpt_eq2. apply eq_refl.
-  + rewrite IHpt_eq. apply eq_refl.
-  + rewrite IHpt_eq1. rewrite IHpt_eq2. apply eq_refl.
-Qed.
-
-Inductive pterm_JMeq : forall {a b : nat}, pterm a -> pterm b -> Prop :=
-| JMeq_var : pterm_JMeq PVar PVar
-| JMeq_abs {n m} (tl : pterm (S n)) (tr : pterm (S m)) :
-  pterm_JMeq tl tr -> pterm_JMeq (PAbs tl) (PAbs tr)
-| JMeq_app {nl nr ml mr} (tl1 : pterm nl) (tl2 : pterm ml) (tr1 : pterm nr) (tr2 : pterm mr) :
-  pterm_JMeq tl1 tr1 -> pterm_JMeq tl2 tr2 -> pterm_JMeq (PApp tl1 tl2) (PApp tr1 tr2)
-| JMeq_sym {n m} (tl : pterm n) (tr : pterm m) : pterm_JMeq tl tr -> pterm_JMeq tr tl
-| JMeq_trans {n m p} (tl : pterm n) (tm : pterm m) (tr : pterm p) :
-  pterm_JMeq tl tm -> pterm_JMeq tm tr -> pterm_JMeq tl tr
-| JMeq_refl {n} (t : pterm n) : pterm_JMeq t t.
-
-Lemma pterm_JMeq_is_eq : forall {a b : nat} (t1 : pterm a) (t2 : pterm b),
-  pterm_JMeq t1 t2 -> a = b.
-Proof. intros a b t1 t2 h.
-induction h.
-- reflexivity.
-- apply (eq_add_S n m IHh).
-- apply (f_equal2_plus nl nr ml mr IHh1 IHh2).
-- apply (Nat.eq_sym IHh).
-- apply (Nat.eq_trans IHh1 IHh2).
-- reflexivity.
-Qed.
-
-Import EqNotations.
-
-Lemma pterm_JMeq_refl : forall {a : nat} (t : pterm a), pterm_JMeq t t.
+Lemma subst_ctx_app : forall {n m : nat} (rul : s_rule) (ctx1 : context n) (ctx2 : context m),
+  subst1_ctx rul (ctx1 ++ ctx2) = subst1_ctx rul ctx1 ++ subst1_ctx rul ctx2.
 Admitted.
-
-Check eq_ind.
-
-Lemma pt_ind : forall {n : nat} {x : pterm n} (P : forall {n' : nat}, pterm n' -> Prop),
-  P x -> forall {m : nat} {y : pterm m}, n = m -> JMeq x y -> P y.
-Admitted.
-
-Lemma pt_JMeq : forall {a b : nat} (t1 : pterm a) (t2 : pterm b), JMeq t1 t2 <-> pterm_JMeq t1 t2.
-Admitted.
-
 
 Lemma subst1_typed : forall {n : nat} (rul : s_rule) (ctx : context n) (pt : pterm n) (ty : type),
   is_typed ctx pt ty -> is_typed (subst1_ctx rul ctx) pt (subst1_ty rul ty).
 Proof.
 intro n.
-case_eq n.
-- intro eq. intros rul ctx pt ty.
-  dependent destruction ctx.
-  case ty.
-  case rul.
-  + intros target ty' atom h.
-    dependent destruction pt.
-    dependent destruction h.
-Admitted.
+intros rul ctx pt ty h.
+induction h.
+- simpl. apply typed_var.
+- simpl. apply typed_abs. simpl in IHh. apply IHh.
+- rewrite subst_ctx_app.
+  apply typed_app with (ty1 := subst1_ty rul ty1).
+  simpl in IHh1. apply IHh1. apply IHh2.
+Qed.
 
 Lemma subst_typed : forall {n : nat} (sigma : s_rules) (ctx : context n) (pt : pterm n) (ty : type),
   is_typed ctx pt ty -> is_typed (subst_ctx sigma ctx) pt (subst_ty sigma ty).
@@ -199,9 +133,9 @@ Inductive is_bidir_syn : forall {a : nat}, context a -> sterm a -> type -> const
 | bidir_abs {n} ty1 gamma (st : sterm (S n)) ty2 const :
   is_bidir_syn (gamma :: ty1) st ty2 const -> 
   is_bidir_syn  gamma (SAbs st) (TImp ty1 ty2) const
-| bidir_cir {n} (a : nat) gamma (ct : cterm n) ty const :
-  is_bidir_chk gamma ct ty const ->
-  is_bidir_syn gamma (SCir a ct) ty const
+| bidir_cir {n} (a : nat) gamma (ct : cterm n) const :
+  is_bidir_chk gamma ct (TVar a) const ->
+  is_bidir_syn gamma (SCir a ct) (TVar a) const
 
 with is_bidir_chk : forall {a : nat}, context a -> cterm a -> type -> constraints -> Prop :=
 | bidir_var ty : is_bidir_chk [ty] CVar ty List.nil
@@ -225,16 +159,10 @@ Inductive is_bidir : forall {a : nat}, context a -> lterm a -> type -> constrain
 Lemma typed_lp : forall {a : nat} (ctx : context a) (lt : lterm a) (ty : type) (const : constraints),
   is_bidir ctx lt ty const -> is_typed ctx (strip_labels lt) ty.
 Proof.
-intros a ctx lt ty const.
-induction lt.
-induction s.
-intro h.
-case ty in h.
+intros a ctx lt ty const h.
 induction h.
-- 
-  case_eq ty.
-  + admit.
-  + simpl. apply 
+induction H.
+- Admitted.
 *)
 
 (* ===== Inference ===== *)
